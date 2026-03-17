a31672535ae91071e5d96e2be1d953f5bec1636a
jesseojala20-crypto <jesseojala20@gmail.com>
2026-03-17 11:19:04 +0200

    coding

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingpower = 16f;
+    private bool isfacingright = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float
+    }
+}
